33bd2009aabde3f2e2223880d14645fbe9b9c050
llewelynklaase <llewelyn.klaase@gmail.com>
2022-02-06 22:48:14 +0200

    Added tests

--- src/main/java/za/ac/cput/Domain/Divide.java
+++ src/main/java/za/ac/cput/Domain/Divide.java
@@ -1,4 +1,15 @@
+/**
+ * Divide.java
+ * Author: Llewelyn Klaase
+ * Student no: 216267072
+ * Description: Program to divide two numbers
+ * Date: 06 February 2022
+ */
 package za.ac.cput.Domain;
 
 public class Divide {
+
+    public double answer(double a, double b) {
+        return b / a;
+    }
 }
--- src/test/java/za/ac/cput/Domain/AddTest.java
+++ src/test/java/za/ac/cput/Domain/AddTest.java
@@ -1,17 +1,67 @@
+/**
+ * AddTest.java
+ * Author: Llewelyn Klaase
+ * Student no: 216267072
+ * Description: Program to add two numbers
+ * Date: 06 February 2022
+ */
 package za.ac.cput.Domain;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-
+import org.junit.jupiter.api.*;
+import java.util.concurrent.TimeUnit;
 import static org.junit.jupiter.api.Assertions.*;
+@TestMethodOrder(MethodOrderer.MethodName.class)
 
 class AddTest {
 
+    private Add sum1;
+    private Add sum2;
+    private Add sum3;
+
+    @Order(1)
+    @Test
     @BeforeEach
     void setUp() {
+        this.sum1 = new Add();
+        this.sum2 = new Add();
+        sum3 = sum1;
     }
 
-    @AfterEach
-    void tearDown() {
+    @Order(2)
+    @Test
+    void testEquality(){
+        assertEquals(sum1, sum3);
     }
+
+    @Order(3)
+    @Test
+    void testIdentity(){
+        assertSame(sum1, sum3);
+    }
+
+    @Order(4)
+    @Test
+    void failTest() {
+        fail("This test will fail");
+    }
+
+    @Order(5)
+    @Test
+    @Timeout(value = 3, unit = TimeUnit.SECONDS)
+    void testThis(){
+        System.out.println("Test time out");
+    }
+
+    @Order(6)
+    @Test
+    @Disabled("Method not needed.")
+    void afterAll() {
+
+    }
+
+    @Order(7)
+    @Test
+    @AfterEach
+    void tearDown() {}
+
 }
