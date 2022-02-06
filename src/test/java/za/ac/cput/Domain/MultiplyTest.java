6e8257355748447f7dcc503989289f3fb7b42055
llewelynklaase <llewelyn.klaase@gmail.com>
2022-02-06 20:53:51 +0200

    Initial commit

---
+++ src/test/java/za/ac/cput/Domain/MultiplyTest.java
@@ -0,0 +1,17 @@
+package za.ac.cput.Domain;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MultiplyTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
---
+++ src/test/java/za/ac/cput/Domain/SubtractTest.java
@@ -0,0 +1,17 @@
+package za.ac.cput.Domain;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SubtractTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
